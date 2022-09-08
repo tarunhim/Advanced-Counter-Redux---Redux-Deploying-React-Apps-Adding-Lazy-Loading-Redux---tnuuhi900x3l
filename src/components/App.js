d74e11ac23beac30cbd748c033f4ec721a5b465d
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-09-08 18:35:55 +0530

    solve

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { increase, signin, signout, decrease, increaseByAmount } from '../actions/action';
 
 function App() {
-  const [input,setInput] = useState(0);
+  const [input,setInput] = useState(2);
   const {counter, islogged} = useSelector(state => state);
   const dispatch = useDispatch();
   return (
@@ -12,7 +12,7 @@ function App() {
       {islogged && 
       <>
       <button onClick={() => dispatch({ type:increase })}>+</button>
-      <span>{counter}</span>
+      <span data-testid='counter' >{counter}</span>
       <button onClick={() => dispatch({type:decrease})} >-</button>
       <input onChange={(e) => setInput(e.target.value)} type="number" value={input} />
       <button onClick={() => dispatch( {type: increaseByAmount, payload:input} )} >Add amount</button>
